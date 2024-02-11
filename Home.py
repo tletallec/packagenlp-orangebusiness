dccdfd62c0f9eb12e12467cea7e06ba9442cf866
tletallec <110519717+tletallec@users.noreply.github.com>
2024-02-11 15:42:27 +0000

    first

---
+++ Home.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import importlib.resources as resources
+
+
+from PIL import Image
+
+st.set_page_config(
+    page_title="Acceuil",
+    page_icon="👋",
+    layout = "wide"
+)
+
+resource_logo = "logo.png"
+chemin_image = resources.files('resources').joinpath(resource_logo)
+image = Image.open(chemin_image)
+st.sidebar.image(image)
+
+st.title("Package NLP")
+st.header("Informations")
+st.markdown("Initié le : 02/03/2023")
+st.markdown("Interlocuteurs : Marine CERCLIER, Grégory GAUTHIER, Tangi LE TALLEC, Alan BIGNON, Islem EZZINE, Killian FARRELL")
+st.markdown("Dans le cadre du projet interne DataScience Package NLP")
+
+st.header("Présentation de la classe NLP")
+st.write("La classe NLP fournit un ensemble de méthodes pour le prétraitement du texte dans les langues française et anglaise. Voici une brève présentation de ses fonctionnalités principales:")
+st.write("- **cleanStopWord**: Cette méthode permet de nettoyer les mots d'arrêt dans le texte en fonction de la langue choisie et offre la possibilité d'ajouter ou de supprimer des mots d'arrêt spécifiques.")
+st.write("- **cleanText**: Cette méthode polyvalente nettoie un texte en supprimant tous les caractères spéciaux, avec des options pour garder les nombres et certains caractères exceptionnels.")
+st.write("- **lemmatisation**: Une méthode avancée qui applique la lemmatisation sur le texte, avec des options pour exclure certains mots ou types de mots et garder les nombres.")
+st.write("\nEnsemble, ces méthodes facilitent grandement le prétraitement du texte, ce qui est une étape cruciale dans de nombreux projets de traitement du langage naturel (NLP). La classe est conçue pour être facilement personnalisable et adaptable aux différents besoins et exigences des projets NLP.")
---
+++ pages/Data_Processing.py
@@ -0,0 +1,100 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+import numpy as np
+import importlib.resources as resources
+
+from wordcloud import WordCloud, ImageColorGenerator
+from PIL import Image
+from collections import Counter
+from packagenlp.nlp import NLP
+
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+dataframe = None
+nlp = NLP()
+
+tab_titles = ['Import Data', 'Treatment Data', 'Visualization Data']
+tab1, tab2, tab3 = st.tabs(tab_titles)
+
+with tab1:
+    st.header("📂 Import Data")
+    st.divider()
+    uploaded_file = st.file_uploader("Choose a file")
+
+    if uploaded_file is not None:
+        try:
+            delimiter = st.text_input("Enter the delimiter:", ';')
+            dataframe = pd.read_csv(uploaded_file, delimiter=delimiter)
+            st.write("Data extract:")
+
+            show_all_data = st.toggle("Show all data")
+            if show_all_data:
+                st.write(dataframe)
+            else:
+                st.write(dataframe.head())
+
+            st.write(f"DataFrame Dimensions: {dataframe.shape}")
+
+            language = st.radio("Select the language of your data",
+                             ["**French**", "**English**"],
+                             horizontal=True,
+                             index=None)
+            
+            if language == "French":
+                language = "fr"
+            elif language == "English":
+                language = "en"
+  
+        except Exception as e:
+            st.error(f"Error parsing CSV file: {str(e)}")
+    
+
+
+with tab2:
+    st.header("⚙️ Treatment Data")
+    st.divider()
+
+    if dataframe is not None:
+        selected_column = st.selectbox("Select a column to apply the treatment", dataframe.columns)
+
+        choices_treatment = st.multiselect('Select the treatments to apply to your column',
+                                           ['Clean Text', 'StopWords', 'Lematization'])
+
+        st.divider()
+
+        if 'Clean Text' in choices_treatment:
+            st.subheader("Clean Text")
+
+            st.caption("##### Paramètres")
+            keep_numbers = st.checkbox("Garder les chiffres", value=True)
+            exception = st.text_input("Exceptions", "")
+            remove_accent = st.checkbox("Supprimer les accents", value=True)
+            lowercase = st.checkbox("Texte en minuscule", value=True)
+
+            # Option pour décider si afficher ou non le dataframe après le traitement
+            show_processed_data = st.checkbox("Show processed data")
+
+            apply_clean_text = st.toggle("Apply Clean Text")
+            if apply_clean_text:
+                # Appliquer le nettoyage du texte à la colonne sélectionnée
+                if selected_column is not None and selected_column in dataframe.columns and dataframe[selected_column].apply(lambda x: isinstance(x, str)).all():
+                    with st.spinner("Clean Text in progress"):
+                        dataframe['Cleaned Column'] = dataframe[selected_column].apply(lambda text: nlp.cleanText(text, keep_numbers, exception, remove_accent, lowercase))
+                    st.success('Done!')
+                else:
+                    st.warning("La colonne sélectionnée ne contient pas de texte (chaînes de caractères). Veuillez sélectionner une colonne valide.")
+
+                # Affichage du dataframe après traitement si l'option est cochée
+                if show_processed_data:
+                    st.write("Processed Data extract:")
+                    if 'Cleaned Column' in dataframe.columns:
+                        st.write(dataframe[['Cleaned Column']].head())
+                    else:
+                        st.warning("Please apply Clean Text first.")
+
+        else:
+            st.warning("No DataFrame uploaded yet. Please upload a file in the 'Import Data' tab.")
